d9cc2d66be7748b83b024b452b7c278bf2ab62f0
fluffysheepinx <fluffysheepinx@gmail.com>
2023-01-30 15:41:47 +0900

    都市内政画面追加

---
+++ WPF_Successor_001_to_Vahren/UserControl080_InternalAffairs.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Successor_001_to_Vahren
+{
+    /// <summary>
+    /// UserControl080_InternalAffairs.xaml の相互作用ロジック
+    /// </summary>
+    public partial class UserControl080_InternalAffairs : UserControl
+    {
+        public UserControl080_InternalAffairs()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = (MainWindow)Application.Current.MainWindow;
+            if (mainWindow == null)
+            {
+                return;
+            }
+
+            // キャンバスから自身を取り除く
+            mainWindow.canvasUI.Children.Remove(this);
+
+            // ルーティングを処理済みとしてマークする（親コントロールのイベントが発生しなくなる）
+            e.Handled = true;
+        }
+    }
+}
