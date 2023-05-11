4771c115d9652f720bac0be455ea64c47bf663e2
fluffysheepinx <fluffysheepinx@gmail.com>
2023-05-11 17:49:24 +0900

    新しいゲームエンジンで書き直し

---
+++ OS3_LoganToga/Main.cpp
@@ -0,0 +1,55 @@
+﻿# include <Siv3D.hpp> // OpenSiv3D v0.6.9
+
+const bool debug = true;
+const String newlineCode = U"\r\n";
+// ウィンドウの幅
+const int32 WindowSizeWidth = 1920;
+// ウィンドウの高さ
+const int32 WindowSizeHeight = 1017;
+const String PathImage = U"image0001";
+const String PathMusic = U"music001";
+const String PathSound = U"sound001";
+
+
+/// @brief 共有するデータ構造体
+struct GameData
+{
+
+};
+
+using App = SceneManager<String, GameData>;
+
+void Main()
+{
+	// ウィンドウのタイトル | Window title
+	Window::SetTitle(U"MagicArc-Ver0.1");
+
+	// ウィンドウのサイズ | Window size
+	Window::Resize(WindowSizeWidth, WindowSizeHeight);
+	//// フルスクリーンモードのデフォルトを有効にするか | Whether to enable fullscreen mode by default
+	//Window::SetFullscreen(true);
+	// 背景の色を設定する | Set the background color
+	//Scene::SetBackground(ColorF{ 0.6, 0.8, 0.7 });
+	// ウィンドウを中心に移動
+	Window::Centering();
+	//ウィンドウを手動でリサイズできるようにする
+	//ウィンドウの最大化を可能にする
+	Window::SetStyle(WindowStyle::Sizable);
+
+	Scene::SetResizeMode(ResizeMode::Virtual);
+	// ウィンドウを最大化
+	Window::Maximize();
+
+	// シーンマネージャーを作成
+	// ここで GameData が初期化される
+	App manager;
+
+
+	while (System::Update())
+	{
+		if (not manager.update())
+		{
+			break;
+		}
+	}
+}
